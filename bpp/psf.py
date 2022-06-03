e7f0deb41238c36be5aff313247565ce8fc2a9a1
Ismael Mendoza <imendoza@umich.edu>
2022-06-03 19:31:37 -0400

    pylint

--- bpp/psf.py
+++ bpp/psf.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def get_gaussian_psf(fwhm: float) -> galsim.GSObject:
+    """Return a Gaussian PSF model specified with FWHM."""
     return galsim.Gaussian(fwhm=fwhm, flux=1.0)
 
 
@@ -64,4 +65,4 @@ def get_optical_and_atmospheric_psf(
         psf = optical_psf
     else:
         raise RuntimeError("Neither component is defined.")
-    return psf.withFlux(1.0)
+    return psf.withFlux(1.0)  # pylint: disable=no-value-for-parameter
