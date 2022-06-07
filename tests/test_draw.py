400110b64ad53d23a2e6f711ff161c138176309e
Ismael Mendoza <imendoza@umich.edu>
2022-06-07 12:58:49 -0400

    update test

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -1,9 +1,10 @@
 from bpp.catalog import create_uniform_catalog
 from bpp.psf import get_gaussian_psf
-from bpp.scene import create_scene
+from bpp.scene import create_gaussian_scene
 
 
 def test_draw_galaxy():
+    """Test drawing scene."""
     cat = create_uniform_catalog()
     psf = get_gaussian_psf(fwhm=0.7)
-    create_scene(53, cat, psf, 0.2, 0, 0, 0, 0)
+    create_gaussian_scene(53, cat, psf, 0.2, 0, 0, 0, 0)
