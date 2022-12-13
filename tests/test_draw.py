e83da60dfd8d7b3f7e0a5f19eeccdc67eec89adc
Ismael Mendoza <imendoza@umich.edu>
2022-12-13 14:28:55 -0500

    need a positive sky level now

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -8,5 +8,6 @@ def test_draw_galaxy():
     cat = create_uniform_catalog(n_rows=5)
     validate_catalog(cat)
     psf = get_gaussian_psf(fwhm=0.7)
-    cutouts = create_gaussian_cutouts(53, cat, psf, 0.2, 0, 0, 0, 0)
+    sky_level = 100
+    cutouts = create_gaussian_cutouts(53, cat, psf, 0.2, 0, 0, sky_level, 0)
     assert cutouts.shape == (5, 53, 53)
